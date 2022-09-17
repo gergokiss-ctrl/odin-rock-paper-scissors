1b49d7ac0f1ea167af9ee64cc6fd50ffd84cb64d
gergokiss-ctrl <kiss.gergo55@gmail.com>
2022-09-17 13:07:21 +0200

    Add pseudocode in Hungarian

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
 console.log("test");
+
+/*
+- input ablak felugrik
+- felhasználó beírja a 3 opció közül valamelyiket
+- a prompt addig aktív, amíg a 3 opció közül nem lett beírva az egyik helyesen (kis- és nagybetűkre nem érzékeny)
+- a gép generál egy választ
+- összehasonlítjuk a két választ, és eldöntjük, hogy ki nyert
+- a győztes pontszámát növeljük
+- ha döntetlen, akkor a kör megismétlődik, a pontszámok nem változnak
+- addig fut a játék, amíg 5 kör le nem ment
+- az 5. kör végén kiértékeljük a győztest a pontszámok alapján
+*/
